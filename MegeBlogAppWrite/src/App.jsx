2e5c81c04f3090531e7d6b7f94ef1d4a9986f625
hemant <hemantrayte25@gmail.com>
2025-08-24 20:29:52 +0530

    basic setup and header and footer setup complete

--- MegeBlogAppWrite/src/App.jsx
+++ MegeBlogAppWrite/src/App.jsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import { login, logout } from './store/authSlice'
+import authService from "./store/authSlice"
+import { Footer, Header } from './components'
+
+
 
 function App() {
- 
+ const [loading, setLoading] = useState(true)
+ const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Hemant rayte blog application</h1>
-    </>
-  )
+ useEffect(() => {
+  authService.getCurrentUser()
+  .then((userData) => {
+    if (userData) {
+      dispatch(login({userData}))
+    } else {
+      dispatch(logout());
+    }
+  })
+  .finally(() =>setLoading(false));
+},[])
+
+return !loading ? <>
+<div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header />
+      <main>
+       {/* Siya Ram <Outlet /> */}
+      </main>
+     <Footer />
+    </div>
+</div>
+</>
+
+: null
 }
 
 export default App
