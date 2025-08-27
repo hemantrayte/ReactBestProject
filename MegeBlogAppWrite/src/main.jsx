1ab1ecac4105e818749fa50c6b2d813e9dcd5ffb
hemant <hemantrayte25@gmail.com>
2025-08-27 15:50:16 +0530

    addpost , allposts, editposts, home ,login, post, signup pages and routing complete

--- MegeBlogAppWrite/src/main.jsx
+++ MegeBlogAppWrite/src/main.jsx
@@ -4,10 +4,78 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
+import AllPosts from './pages/AllPosts.jsx'
+import AddPost from './pages/AddPost.jsx'
+import EditPage from './pages/EditPost.jsx'
+import Post from './pages/Post.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: (
+                <Protected authentication={false}>
+                    <Login />
+                </Protected>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <Protected authentication={false}>
+                    <Signup/>
+                </Protected>
+            ),
+        },
+        {
+            path: "/all-posts",
+            element: (
+                <Protected authentication>
+                    {" "}
+                    <AllPosts />
+                </Protected>
+            ),
+        },
+        {
+            path: "/add-post",
+            element: (
+                <Protected authentication>
+                    {" "}
+                    <AddPost />
+                </Protected>
+            ),
+        },
+        {
+            path: "/edit-post/:slug",
+            element: (
+                <Protected authentication>
+                    {" "}
+                    <EditPage/>
+                </Protected>
+            ),
+        },
+        {
+            path: "/post/:slug",
+            element: <Post />,
+        },
+    ],
+},
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <RouterProvider router={router} />
     <App />
     </Provider>
   </StrictMode>,
